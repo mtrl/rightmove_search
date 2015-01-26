2f3436a064b4243c9fa15fac5a4f64acd185d0e3
Mark Williams <mark@mtrl.co.uk>
2015-01-26 14:22:35 +0000

    Added templates

--- rightmove_search.py
+++ rightmove_search.py
@@ -1,4 +1,5 @@
 import urllib2
+import shutil
 import time
 import hashlib
 import webbrowser
@@ -19,7 +20,8 @@ def main():
     wait_time = 1 # Pause for x seconds between requests
     user_agent = 'Mozilla/5 (Solaris 10) Gecko'
     headers = { 'User-Agent' : user_agent }
-
+    
+    copy_templates(output_dir)
     output_file = output_file + "_" + sanitise_filename(str(search_home[0])) + ".html"
     output_path = output_dir + output_file
 
@@ -33,12 +35,22 @@ def main():
     page_urls = [search_home[1]]
     for page_link in page_links:
         page_urls.append(page_link['href'])
+    
+    with open("template/head.html", "r") as myfile:
+        head_html = myfile.read().replace('\n', '')
+        head_html = head_html.replace("[[TITLE]]", search_home[0])
+        
+    with open("template/foot.html", "r") as myfile:
+        foot_html = myfile.read().replace('\n', '')
 
     with open(output_path, "w") as text_file:
         #text_file.write('<meta http-equiv="refresh" content="10">')
+        text_file.write(head_html)
+        text_file.write('<div class="row"><div class="col-md-12">')
         text_file.write("<h1>" + search_home[0] + "</h1>")
         text_file.write("<h2>Found the following properties with the keywords you are looking for</h2>")
         text_file.write("<p><a href='" + root_url + search_home[1] + "' target='_blank'>This is the original search page</a></p>")
+        text_file.write('</div></div>')
 
     # open a browser tab showing the results
     webbrowser.open('file:///' + str(output_dir) + str(output_file), new=2)
@@ -102,9 +114,15 @@ def main():
             price = str(property_html.find('p', { 'id': 'propertyHeaderPrice'}).find('strong').text)
             title = str(property_html.find('h1', { 'class': 'fs-22'}).text)
             subtitle = str(property_html.find('div', { 'class': 'property-header-bedroom-and-price'}).find('address').text)
-
+            property_anchor = '<a href="' + root_url + property_link + '" target="_blank">'
+            
             with open(output_path, "a") as text_file:
-                text_file.write('<p>' + str(j) + ' <a href="' + root_url + property_link + '" target="_blank"><strong>' + title + '</strong><br>' + subtitle + '<br>' + price + '<br>' + img + '</a><br>' + str(results) + '</p>')
+                text_file.write('<div class="row">')
+                text_file.write('<div class="col-md-4">' + property_anchor + '<strong>' + title + '</strong><br>' + subtitle + '</a></div>')
+                text_file.write('<div class="col-md-1">' + property_anchor + price + '</a></div>')
+                text_file.write('<div class="col-md-4">' + property_anchor + img + '</a></div>')
+                text_file.write('<div class="col-md-3">' + str(results) + '</div>')
+                text_file.write('</div>')
 
             property_results.append(result)
             j = j + 1
@@ -112,6 +130,9 @@ def main():
 
     print "---"
     print "Done. Found " + str(len(property_results)) + " properties. I've written the results to " + output_dir
+    
+    with open(output_path, "a") as text_file:
+        text_file.write(foot_html)
 
 def search(text,n):
     #Searches for text, and retrieves n words either side of the text, which are retuned seperatly
@@ -121,6 +142,10 @@ def search(text,n):
     
 def sanitise_filename(filename):
     return "".join([c for c in filename if c.isalpha() or c.isdigit() or c==' ']).rstrip().replace(" ", "_")
+
+def copy_templates(output_dir):
+    shutil.rmtree(output_dir + "template", True)
+    shutil.copytree('template', output_dir + 'template')
 
 if __name__ == "__main__":
     main()
